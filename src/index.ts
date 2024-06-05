39e3726475fb077b105637b05e96420ac6188e83
gabriel <gabrielcorreiaferrera2001@gmail.com>
2024-06-05 23:50:46 +0000

    2 - Added a linter and formatter to the project using ts-standard and standardJS.

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ const app = express()
 
 app.use(express.json()) // Middleware transform reqbody into json.
 
-const PORT = 3000;
+const PORT = 3000
 
-app.get("/salute", (_req, res) => {
-    console.log("lol")
-    res.send("Hello")
+app.get('/salute', (_req, res) => {
+  console.log('lol')
+  res.send('Hello')
 })
 
 app.listen(PORT, () => {
-    console.log('App listen in port: 3000')
+  console.log('App listen in port: 3000')
 })
