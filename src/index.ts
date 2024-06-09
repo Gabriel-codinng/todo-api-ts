72d100a33290604d001af38bbbf66739103aea86
gabriel <gabrielcorreiaferrera2001@gmail.com>
2024-06-09 19:24:39 +0000

    3. Add configurations and uninstall unnecessary dependencies.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,1 @@
-import express from 'express'
-
-const app = express()
-
-app.use(express.json()) // Middleware transform reqbody into json.
-
-const PORT = 3000
-
-app.get('/salute', (_req, res) => {
-  console.log('lol')
-  res.send('Hello')
-})
-
-app.listen(PORT, () => {
-  console.log('App listen in port: 3000')
-})
+console.log('Hello world!')
